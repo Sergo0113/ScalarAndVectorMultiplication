65046e1b5f283cfbf7e49de0cbcd8201778ea0cf
Xyrex6 <xyrex2014@yandex.ru>
2023-12-03 21:20:18 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace WinFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox6 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox2 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox3 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            label9 = new Label();
+            button6 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(553, 73);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 54);
+            button1.TabIndex = 0;
+            button1.Text = "Экспорт в Excel";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(182, 107);
+            label1.Name = "label1";
+            label1.Size = new Size(22, 20);
+            label1.TabIndex = 1;
+            label1.Text = "a(";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(181, 162);
+            label2.Name = "label2";
+            label2.Size = new Size(23, 20);
+            label2.TabIndex = 2;
+            label2.Text = "b(";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(210, 104);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(47, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(352, 159);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(47, 27);
+            textBox6.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(263, 107);
+            label3.Name = "label3";
+            label3.Size = new Size(12, 20);
+            label3.TabIndex = 5;
+            label3.Text = ";";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(263, 162);
+            label4.Name = "label4";
+            label4.Size = new Size(12, 20);
+            label4.TabIndex = 6;
+            label4.Text = ";";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(334, 107);
+            label5.Name = "label5";
+            label5.Size = new Size(12, 20);
+            label5.TabIndex = 7;
+            label5.Text = ";";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(334, 162);
+            label6.Name = "label6";
+            label6.Size = new Size(12, 20);
+            label6.TabIndex = 8;
+            label6.Text = ";";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(281, 104);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(47, 27);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(210, 159);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(47, 27);
+            textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(281, 159);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(47, 27);
+            textBox5.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(352, 104);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(47, 27);
+            textBox3.TabIndex = 12;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(405, 107);
+            label7.Name = "label7";
+            label7.Size = new Size(14, 20);
+            label7.TabIndex = 13;
+            label7.Text = ")";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(405, 162);
+            label8.Name = "label8";
+            label8.Size = new Size(14, 20);
+            label8.TabIndex = 14;
+            label8.Text = ")";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(169, 202);
+            button2.Name = "button2";
+            button2.Size = new Size(122, 71);
+            button2.TabIndex = 15;
+            button2.Text = "Векторное произведение";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(318, 202);
+            button3.Name = "button3";
+            button3.Size = new Size(122, 71);
+            button3.TabIndex = 16;
+            button3.Text = "Скалярное произведение";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(553, 145);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 54);
+            button4.TabIndex = 17;
+            button4.Text = "Экспорт в Word";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(553, 219);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 54);
+            button5.TabIndex = 18;
+            button5.Text = "Экспорт в PDF";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(263, 73);
+            label9.Name = "label9";
+            label9.Size = new Size(0, 20);
+            label9.TabIndex = 19;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(553, 296);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 29);
+            button6.TabIndex = 20;
+            button6.Text = "Очистить";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button6);
+            Controls.Add(label9);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(textBox3);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox2);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox6);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox6;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox2;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox3;
+        private Label label7;
+        private Label label8;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Label label9;
+        private Button button6;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System.IO;
+using ClosedXML.Excel;
+using Aspose.Cells;
+using System.Linq.Expressions;
+
+namespace WinFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        double x1, x2, y1, y2, z1, z2, scalaranswer, vectoranswer1, vectoranswer2, vectoranswer3;
+        bool scalar = false, vector = false;
+        string message = "Заполните числами все ячейки!", title = "Ошибка", message1 = "Числа -0 не существует!";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (vector == true || scalar == true)
+            {
+                excel();
+                Workbook workbook = new Workbook("excel.xlsx");
+                SaveFileDialog sfd = new SaveFileDialog();
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    string filepath = sfd.FileName + ".xlsx";
+                    workbook.Save(filepath, SaveFormat.Xlsx);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "-0" && textBox2.Text != "-0" && textBox3.Text != "-0" && textBox4.Text != "-0" && textBox5.Text != "-0" && textBox6.Text != "-0")
+            {
+                try
+                {
+                    scalar = false; vector = true;
+                    x1 = Convert.ToDouble(textBox1.Text);
+                    x2 = Convert.ToDouble(textBox4.Text);
+                    y1 = Convert.ToDouble(textBox2.Text);
+                    y2 = Convert.ToDouble(textBox5.Text);
+                    z1 = Convert.ToDouble(textBox3.Text);
+                    z2 = Convert.ToDouble(textBox6.Text);
+                    vectoranswer1 = (y1 * z2 - z1 * y2);
+                    vectoranswer2 = -(x1 * z2 - z1 * x2);
+                    if (Convert.ToString(vectoranswer2) == "-0")
+                        vectoranswer2 = 0;
+                    vectoranswer3 = (x1 * y2 - y1 * x2);
+                    vectoranswer1 = Math.Round(vectoranswer1, 10);
+                    vectoranswer2 = Math.Round(vectoranswer2, 10);
+                    vectoranswer3 = Math.Round(vectoranswer3, 10);
+                    label9.Text = "(" + vectoranswer1 + " ; " + vectoranswer2 + " ; " + vectoranswer3 + ")";
+                }
+                catch
+                {
+                    MessageBox.Show(message, title);
+                }
+            }
+            else
+                MessageBox.Show(message1, title);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "-0" && textBox2.Text != "-0" && textBox3.Text != "-0" && textBox4.Text != "-0" && textBox5.Text != "-0" && textBox6.Text != "-0")
+            {
+                try
+                {
+                    vector = false; scalar = true;
+                    x1 = Convert.ToDouble(textBox1.Text);
+                    x2 = Convert.ToDouble(textBox4.Text);
+                    y1 = Convert.ToDouble(textBox2.Text);
+                    y2 = Convert.ToDouble(textBox5.Text);
+                    z1 = Convert.ToDouble(textBox3.Text);
+                    z2 = Convert.ToDouble(textBox6.Text);
+                    scalaranswer = x1 * x2 + y1 * y2 + z1 * z2;
+                    label9.Text = Convert.ToString(scalaranswer);
+                }
+                catch
+                {
+                    MessageBox.Show(message, title);
+                }
+            }
+            else
+                MessageBox.Show(message1, title);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (vector == true || scalar == true)
+            {
+                excel();
+                Workbook workbook = new Workbook("excel.xlsx");
+
+                SaveFileDialog sfd = new SaveFileDialog();
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    string filepath = sfd.FileName + ".docx";
+                    workbook.Save(filepath, SaveFormat.Docx);
+
+                }
+            }
+
+        }
+
+        void excel()
+        {
+            if (scalar)
+            {
+                var workbook = new XLWorkbook();
+                var worksheet = workbook.Worksheets.Add("Лист1");
+
+                worksheet.Cell("A" + 1).Value = "Ответ:";
+                worksheet.Cell("A" + 2).Value = "a · b = " + Convert.ToString(scalaranswer);
+                worksheet.Cell("A" + 3).Value = "Ход решения";
+                worksheet.Cell("A" + 4).Value = "Вычисляем по формуле: a · b = x1 · x2 + y1 · y2 + z1 · z2, соответственно";
+                worksheet.Cell("A" + 5).Value = "a · b = " + x1 + " · " + x2 + " + " + y1 + " · " + y2 + " + " + z1 + " · " + z2 + " = " + x1 * x2 + " + " + y1 * y2 + " + " + z1 * z2;
+                worksheet.Cell("A" + 6).Value = "И получаем ответ: " + scalaranswer;
+
+
+                worksheet.Columns().AdjustToContents(); //ширина столбца по содержимому
+
+                workbook.SaveAs("excel.xlsx");
+            }
+            if (vector)
+            {
+                var workbook = new XLWorkbook();
+                var worksheet = workbook.Worksheets.Add("Лист1");
+
+                worksheet.Cell("A" + 1).Value = "Ответ:";
+                worksheet.Cell("A" + 2).Value = "a × b = ( " + vectoranswer1 + " ; " + vectoranswer2 + " ; " + vectoranswer3 + " )";
+                worksheet.Cell("A" + 3).Value = "Вычисляем по формуле: a × b = ";
+                worksheet.Cell("B" + 2).Value = "i";
+                worksheet.Cell("C" + 2).Value = "j";
+                worksheet.Cell("D" + 2).Value = "k";
+                worksheet.Cell("F" + 2).Value = "y1";
+                worksheet.Cell("G" + 2).Value = "z1";
+                worksheet.Cell("I" + 2).Value = "x1";
+                worksheet.Cell("J" + 2).Value = "z1";
+                worksheet.Cell("L" + 2).Value = "x1";
+                worksheet.Cell("M" + 2).Value = "y1";
+                worksheet.Cell("B" + 3).Value = "x1";
+                worksheet.Cell("C" + 3).Value = "y1";
+                worksheet.Cell("D" + 3).Value = "z1";
+                worksheet.Cell("E" + 3).Value = "=";
+                worksheet.Cell("F" + 3).Value = "y2";
+                worksheet.Cell("G" + 3).Value = "z2";
+                worksheet.Cell("H" + 3).Value = "i - ";
+                worksheet.Cell("I" + 3).Value = "x2";
+                worksheet.Cell("J" + 3).Value = "z2";
+                worksheet.Cell("K" + 3).Value = "j + ";
+                worksheet.Cell("L" + 3).Value = "x2";
+                worksheet.Cell("M" + 3).Value = "y2";
+                worksheet.Cell("N" + 3).Value = "k";
+                worksheet.Cell("B" + 4).Value = "x2";
+                worksheet.Cell("C" + 4).Value = "y2";
+                worksheet.Cell("D" + 4).Value = "z2";
+                worksheet.Cell("B" + 6).Value = "i";
+                worksheet.Cell("C" + 6).Value = "j";
+                worksheet.Cell("D" + 6).Value = "k";
+                worksheet.Cell("F" + 6).Value = y1;
+                worksheet.Cell("G" + 6).Value = z1;
+                worksheet.Cell("I" + 6).Value = x1;
+                worksheet.Cell("J" + 6).Value = z1;
+                worksheet.Cell("L" + 6).Value = x1;
+                worksheet.Cell("M" + 6).Value = y1;
+                worksheet.Cell("A" + 7).Value = "соответствено, a × b = ";
+                worksheet.Cell("B" + 7).Value = x1;
+                worksheet.Cell("C" + 7).Value = y1;
+                worksheet.Cell("D" + 7).Value = z1;
+                worksheet.Cell("E" + 7).Value = "=";
+                worksheet.Cell("F" + 7).Value = y2;
+                worksheet.Cell("G" + 7).Value = z2;
+                worksheet.Cell("H" + 7).Value = "i - ";
+                worksheet.Cell("I" + 7).Value = x2;
+                worksheet.Cell("J" + 7).Value = z2;
+                worksheet.Cell("K" + 7).Value = "j + ";
+                worksheet.Cell("L" + 7).Value = x2;
+                worksheet.Cell("M" + 7).Value = y2;
+                worksheet.Cell("N" + 7).Value = "k =";
+                if (textBox2.Text != "0" && textBox5.Text != "0")
+                    worksheet.Cell("O" + 7).Value = "(" + y1 * z2 + " - " + z1 * y2 + ") - (" + x1 * z2 + " - " + z1 * x2 + ") + (" + x1 * y2 + " - " + y1 * x2 + ") = " + vectoranswer1 + " - " + vectoranswer2 + " + " + vectoranswer3;
+                worksheet.Cell("B" + 8).Value = x2;
+                worksheet.Cell("C" + 8).Value = y2;
+                worksheet.Cell("D" + 8).Value = z2;
+                worksheet.Cell("A" + 9).Value = "И получаем ответ: (" + vectoranswer1 + " ; " + vectoranswer2 + " ; " + vectoranswer3 + ")";
+                var rngTable = worksheet.Range("B2:B" + 4);
+                rngTable.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                var rngTable1 = worksheet.Range("D2:D" + 4);
+                rngTable1.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                var rngTable2 = worksheet.Range("F2:F" + 3);
+                rngTable2.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                var rngTable3 = worksheet.Range("G2:G" + 3);
+                rngTable3.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                var rngTable4 = worksheet.Range("I2:I" + 3);
+                rngTable4.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                var rngTable5 = worksheet.Range("J2:J" + 3);
+                rngTable5.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                var rngTable6 = worksheet.Range("L2:L" + 3);
+                rngTable6.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                var rngTable7 = worksheet.Range("M2:M" + 3);
+                rngTable7.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                var rngTable8 = worksheet.Range("B6:B" + 8);
+                rngTable8.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                var rngTable9 = worksheet.Range("D6:D" + 8);
+                rngTable9.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                var rngTable10 = worksheet.Range("F6:F" + 7);
+                rngTable10.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                var rngTable11 = worksheet.Range("G6:G" + 7);
+                rngTable11.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                var rngTable12 = worksheet.Range("I6:I" + 7);
+                rngTable12.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                var rngTable13 = worksheet.Range("J6:J" + 7);
+                rngTable13.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+                var rngTable14 = worksheet.Range("L6:L" + 7);
+                rngTable14.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+                var rngTable15 = worksheet.Range("M6:M" + 7);
+                rngTable15.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                worksheet.Columns().AdjustToContents();
+                worksheet.Rows().AdjustToContents();
+
+                workbook.SaveAs("excel.xlsx");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (vector == true || scalar == true)
+            {
+                excel();
+                Workbook workbook = new Workbook("excel.xlsx");
+
+                SaveFileDialog sfd = new SaveFileDialog();
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    string filepath = sfd.FileName + ".pdf";
+                    workbook.Save(filepath, SaveFormat.Pdf);
+
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            label9.Text = "";
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
